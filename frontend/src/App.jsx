402b44396b0e5cfe8a073f76d8e3dd966a48f180
Camillia <camilliaetr@gmail.com>
2026-04-07 11:05:56 +0200

    merge des features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import VisitorForm from "./features/visitor-form/components/VisitorForm";
+import LoginForm from "./features/auth/components/LoginForm";
 
 export default function App() {
   return <VisitorForm />;
